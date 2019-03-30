42d7e95667789640467cc3b73cffe8bd44e83eee
vlkm <46622384+vlkm@users.noreply.github.com>
2019-03-30 23:10:24 +0200

    static Hello World with and without return

--- src/main/java/com/HelloWorld/App.java
+++ src/main/java/com/HelloWorld/App.java
@@ -1,5 +1,7 @@
 package com.HelloWorld;
 
+
+
 /**
  * Hello world!
  *
@@ -9,5 +11,18 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Hello_World();
+        System.out.println(Hello__World());
+
     }
+
+    public static void Hello_World(){
+
+        System.out.println("Hello World!");
+    }
+
+    private static String Hello__World(){
+        return "Hello World!";
+}
+
 }
